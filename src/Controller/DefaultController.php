6c2928611d086aba073769c6718e5672e0599255
Marek Korcz <mark.korcz@gmail.com>
2018-08-21 06:58:35 +0200

    Working on sending session's picture names using fetch

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,7 +7,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends AbstractController {
    
@@ -52,6 +54,55 @@ class DefaultController extends AbstractController {
         return new JsonResponse(array(
             'type'    => 'error',
             'message' => 'No data'            
+        ));
+    }
+    
+    /**
+     * @Route("/get-photos", name="get_photos")
+     * @Method({"POST"})
+     * 
+     * @return JsonResponse
+     */
+    public function getPhotos(Request $request) {
+        
+        $data = json_decode($request->getContent());
+
+        $session = $request->request->get('sessionName');
+        
+//        $session = $data['sessionName'];
+        
+        if ($session) {
+            
+            $photos = $this->getDoctrine()->getManager()->getRepository('App\Entity\Photograph')->findBy(array(
+                "session" => $session
+            ));
+            
+            if ($photos) {
+
+                $encoders = array(
+                    new JsonEncoder()
+                );
+                $normalizers = array(
+
+                    (new ObjectNormalizer())
+                        ->setCircularReferenceHandler(function($object)
+                        {
+                            return $object->__toString();
+                        })
+                );
+
+                $serializer = new Serializer($normalizers, $encoders);
+
+                $data = $serializer->serialize($photos, 'json');
+
+                return new JsonResponse($data, 200, array(), true);
+            }
+        }
+        
+        return new JsonResponse(array(
+            'type'    => 'error',
+            'message' => 'No data',
+            'session' => $data
         ));
     }
 }
